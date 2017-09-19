9715a7884dd7d55b7b07c64f8adee259fbac9d10
plokmij <samfan@live.com>
2017-09-19 12:57:35 +0530

    Update extractor.py
    Extractor completed

--- extractor.py
+++ extractor.py
@@ -3,4 +3,14 @@ import os
 import sys
 
 for i in range(1,len(sys.argv)):
+	print "Extracting "+str(sys.argv[i]),
 	os.system("dtrx "+str(sys.argv[i]))
+	print "Done"
+	print "Fetching data from "+str(sys.argv[i])[:-3],
+	os.system("./test.py "+str(sys.argv[i])[:-3])
+	print "Done"
+	print "Deleting  "+str(sys.argv[i])[:-3],
+	os.system("rm "+str(sys.argv[i])[:-3])
+	print "Done"
+
+#27843
